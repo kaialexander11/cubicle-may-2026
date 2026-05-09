f2bbd7038c1f36e4c772db6e81db813c2f280e27
Alexander Uzunov <kaialexander.11@gmail.com>
2026-05-09 16:00:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+//console.log('Hello world!');
+
+const express = require('express');
+
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}... `));
