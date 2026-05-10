51cd36bb3152c1364b8eec265f9acf4d9f5e4a08
Alexander Uzunov <kaialexander.11@gmail.com>
2026-05-10 13:35:07 +0300

    Add express configuration
    Add handlebars configuration

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@
 
 const express = require('express');
 const handlebars = require('express-handlebars');
+const expressConfigurator = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
 const path = require('path');
 const app = express();
 
 const PORT = 5000;
 
-// Express config => css + images:
-app.use(express.static(path.resolve(__dirname, 'public')));
+// Express config => css + images: => path.resolve constructs an Absolute Path! 
+// app.use(express.static('src/public')); => this also works correctly!
+// app.use(express.static(path.resolve(__dirname, 'public')));
 
-// Handlebars config!
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
-
+expressConfigurator(app);
+handlebarsConfig(app);
 
 app.get('/', (req, res) => {
     //res.send('Hello from Express!');
