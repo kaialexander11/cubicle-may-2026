3e3b9f1ed245bd1b30b746eafdceb425cf0277c8
Alexander Uzunov <kaialexander.11@gmail.com>
2026-05-13 12:43:38 +0300

    Search form remembers inputs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 
     const cubes = cubeManager.getAll(search, from, to);
 
-    res.render('index', { cubes });
+    res.render('index', { cubes, search, from, to });
 
 });
 
