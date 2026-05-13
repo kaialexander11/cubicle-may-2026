86e0beb12d66308180aa5468097f8ab4ae3a70cc
Alexander Uzunov <kaialexander.11@gmail.com>
2026-05-13 12:05:38 +0300

    Add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,9 +3,15 @@ const cubeManager = require('../managers/cubeManager');
 //const router = express.Router;
 
 router.get('/', (req, res) => {
-    const cubes = cubeManager.getAll();
+
+    //console.log(req.query);
+
+    const { search, from, to } = req.query;
+
+    const cubes = cubeManager.getAll(search, from, to);
 
     res.render('index', { cubes });
+
 });
 
 router.get('/about', (req, res) => {
