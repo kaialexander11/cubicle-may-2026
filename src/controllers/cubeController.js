411b698466718a3566a892328f58b9167766cdf7
Alexander Uzunov <kaialexander.11@gmail.com>
2026-05-21 19:09:15 +0300

    Show no accessory title when there are none

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -51,7 +51,9 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
 
     const accessories = await accessoryManager.getAll().lean();
 
-    res.render('accessory/attach', { cube, accessories });
+    const hasAccessories = accessories.length > 0;
+
+    res.render('accessory/attach', { cube, accessories, hasAccessories });
 
 });
 
